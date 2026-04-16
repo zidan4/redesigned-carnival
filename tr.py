b27d74065235ee6378c01294f3e082a4bd5d5866
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2026-04-16 20:37:40 +0300

    Create tr.py

---
+++ tr.py
@@ -0,0 +1,5 @@
+from sklearn.compose import TransformedTargetRegressor
+
+model = TransformedTargetRegressor(LinearRegression(), transformer=StandardScaler())
+model.fit(housing[["median_income"]], housing_labels)
+predictions = model.predict(some_new_data)
